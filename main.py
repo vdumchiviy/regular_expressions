479f33b37b4445b8c2597f2b694bb70088eaa22b
Maksim Kovtun <brain1999@mail.ru>
2020-11-04 16:50:23 +0300

    Added docstrings

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import re
-# from pprint import pprint
 import csv
 
 contacts_list = list()
-# https://regex101.com/r/HI0qmG/1
-# ^(\+7|8)?([ -]*\(?[ -]*)(\d+)([ -]*\)?[ -]*)([ -]*)(\d+)([ -]*)(\d+)([ -]*)(\d+)
 
 
 def read_csv():
+    """This function reads csv from file
+
+    Returns:
+        [list]: contacts list from file
+    """
     # читаем адресную книгу в формате CSV в список contacts_list
 
     with open("phonebook_raw.csv", encoding="utf-8") as f:
@@ -17,6 +19,11 @@ def read_csv():
 
 
 def save_csv(contacts_list: list):
+    """This function saves contacts list into result file
+
+    Args:
+        contacts_list (list): contacts list for saving
+    """
     with open("phonebook.csv", "w", encoding="utf-8") as f:
         datawriter = csv.writer(f, delimiter=',')
         # Вместо contacts_list подставьте свой список
@@ -24,6 +31,14 @@ def save_csv(contacts_list: list):
 
 
 def replace_fio(contacts_list: list):
+    """This function redistributes three items of contacts list (fio) thats way is:
+        zeroth element contains Family Name
+        first element contains Name
+        third element contains Patronic Name
+
+    Args:
+        contacts_list (list): contacts list
+    """
     for item in contacts_list[1:]:
         fio = item[0] + " " + item[1] + " " + item[2]
         pattern = re.compile(r"\s")
@@ -35,13 +50,17 @@ def replace_fio(contacts_list: list):
 
 
 def replace_phone(contacts_list: list):
+    """This function replace phone number to format +7(999)999-99-99 доб.9999
+
+    Args:
+        contacts_list (list): contact list
+    """
     for item in contacts_list[1:]:
         phone = item[5]
         pattern = re.compile(
             r"^(\+7|8)?([ -]*\(?[ -]*)(\d+)([ -]*\)?[ -]*)([ -]*)(\d+)([ -]*)(\d+)([ -]*)(\d+)([ ёа-я,\(\.]*)(\d*)([ ёа-я,\)\.]*)")
         subset = r"\3\6\8\10\12"
         find_string = pattern.sub(subset, phone)
-        # result = "+" + result[0:1].replace("8", "7")
         result = find_string
         if len(find_string) > 0:
             result = "+7(" + find_string[0:3] + ")" + \
@@ -53,6 +72,14 @@ def replace_phone(contacts_list: list):
 
 
 def roll_contacts(contacts_list: list):
+    """This function roll the dublicate contacts
+
+    Args:
+        contacts_list (list): contacts list
+
+    Returns:
+        [list]: rolled contacts list
+    """
     result = contacts_list[0:1]
     index = 1
     index_new = 0
@@ -77,16 +104,11 @@ def roll_contacts(contacts_list: list):
 
         len_contacts_list = len(contacts_list)
         index += 1
-
-    # print(result)
     return result
-    # for item in contacts_list[1:]:
 
 
 contacts_list = read_csv()
 replace_fio(contacts_list)
 replace_phone(contacts_list)
 contacts_list = roll_contacts(contacts_list)
-# print(contacts_list)
 save_csv(contacts_list)
-# print(contacts_list)
